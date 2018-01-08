73a6b5c845489a12409349c0c484f46bb2b489af
BermetKalmakova <bermetkalmakova@gmail.com>
2018-01-07 20:00:01 -0500

    Fix db stuff

--- dbFunctions.py
+++ dbFunctions.py
@@ -23,7 +23,9 @@ def newGameID():
     max = c.execute("SELECT MAX(gameID) FROM games").fetchall()
     db.commit()
     db.close()
-    return maxID[0][0] + 1
+    if max[0][0] is None:
+        return 0
+    return max[0][0] + 1
 
 def addUser(user, password):
     db = sqlite3.connect(f)
@@ -36,14 +38,23 @@ def addGame(user, whites, blacks):
     db = sqlite3.connect(f)
     c = db.cursor()
     id = newGameID()
-    c.execute("INSERT INTO games VALUES('%s', '%s', '%s', '%s', '%s')" % (id, user, 0, TRUE, ''))
-    c.execute("INSERT INTO decks VALUES('%s', '%s', '%s', '%s')" % (id, whiteDeck, blackDeck, ''))
+    c.execute("INSERT INTO games VALUES('%s', '%s', '%s', '%s', '%s')" % (id, user, 0, 1, ''))
+    c.execute("INSERT INTO decks VALUES('%s', '%s', '%s', '%s')" % (id, whites, blacks, ''))
     db.commit()
     db.close()
 
 def addPlayer(gameID, user):
     db = sqlite3.connect(f)
     c = db.cursor()
-    c.execute("INSERT INTO games VALUES('%s', '%s', '%s', '%s', '%s')" % (gameID, user, 0, FALSE, ''))
+    c.execute("INSERT INTO games VALUES('%s', '%s', '%s', '%s', '%s')" % (gameID, user, 0, 0, ''))
     db.commit()
     db.close()
+
+
+#addGame("Jim", "a,b,c", "a,b,c")
+#addPlayer(0, "Bob")
+#db = sqlite3.connect(f)
+#c = db.cursor()
+#c.execute("SELECT * FROM games")
+#data = c.fetchall()
+#print(data)
